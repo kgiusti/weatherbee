4260ddc2653b7a1f5b86fe06d6ffffa84411479e
Kenneth Giusti <kgiusti@redhat.com>
2023-03-20 13:05:10 -0400

    add argparse, remove debug prints

--- wemos_d1_mini/server.py
+++ wemos_d1_mini/server.py
@@ -37,11 +37,11 @@ def do_get(sock, target, in_file):
     global led
     global bme
 
-    print("GET JSON")
+    # print("GET JSON")
     while True:
         line = in_file.readline().decode()
         if line == '':
-            print("Client closed socket")
+            # print("Client closed socket")
             return
         if line.strip() == "":
             # end of headers
@@ -59,7 +59,7 @@ def do_get(sock, target, in_file):
     sock.sendall(b'Content-type: application/json\r\n')
     sock.sendall(b'Content-length: %d\r\n\r\n' % len(out))
     sock.sendall(out)
-    print("json sent %s" % out)
+    # print("json sent %s" % out)
 
 def do_put(sock, target, in_file):
     global led
@@ -68,29 +68,28 @@ def do_put(sock, target, in_file):
     while True:
         line = in_file.readline().decode()
         if line == '':
-            print("Client closed socket")
+            # print("Client closed socket")
             return
         if line.strip() == "":
             # end of headers
             break;
-    print("READ BODY")
+    # print("READ BODY")
     line = in_file.readline().decode().strip()
     # expect json: {'weatherbee': {'led': 'on|off'}
     try:
-        print("READ: %s" % line)
+        # print("READ: %s" % line)
         data = json.loads(line)
         new_led = data['weatherbee']['led']
-        print("NEW LED: %s" % new_led)
+        # print("NEW LED: %s" % new_led)
         if new_led.upper() == "ON":
             led.on()
         else:
             led.off()
-        print("POST DONE OK")
+        # print("POST DONE OK")
         sock.sendall(b'HTTP/1.0 204 No Content\r\n\r\n')
     except Exception as exc:
-        print("POST FAILED: %s" % exc)
+        # print("POST FAILED: %s" % exc)
         sock.sendall(b"HTTP/1.0 400 Bad Cheezeburger\r\n\r\n")
-    print("PUT DONE")
 
 def handle_client(client):
     client_file = client.makefile('rwb', 0)
@@ -98,13 +97,13 @@ def handle_client(client):
     while True:
         line = client_file.readline().decode()
         if line == '':
-            print("Client closed socket")
+            # print("Client closed socket")
             return
         line = line.strip()
         if len(line) != 0:
             break
 
-    print("REQUEST=%s" % line)
+    # print("REQUEST=%s" % line)
     request = line.split()
     method = request[0].upper()
     target = request[1].upper()
@@ -120,15 +119,16 @@ def run():
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server.bind(('0.0.0.0', 8888))
     server.listen(5)
-    print('listening on port 8888')
+    # print('listening on port 8888')
 
     while True:
         client, addr = server.accept()
         try:
-            print('client connected from', addr)
+            # print('client connected from', addr)
             handle_client(client)
         except Exception as exc:
-            print("Exception: %s" % exc)
+            # print("Exception: %s" % exc)
+            pass
         finally:
             client.close()
 
